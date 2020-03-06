d7dafc3024ed073b674636d33207a0160d410854
mlysy <mlysy@users.noreply.github.com>
2020-03-06 10:39:48 -0500

    fixed test errors

--- tests/testthat/test-NormalToeplitz-grad.R
+++ tests/testthat/test-NormalToeplitz-grad.R
@@ -6,7 +6,7 @@ context("NormalToeplitz - Gradient.")
 nrep <- 10
 
 test_that("The GSchur algorithm returns the correct density", {
-  replicate(n = nrep, expr = {  
+  replicate(n = nrep, expr = {
     test_logdens <- function(mu, alpha, X) {
       f <- test_drift_func(mu, N)
       acf <- test_fbm_acf(alpha, dt, N)
@@ -22,13 +22,11 @@ test_that("The GSchur algorithm returns the correct density", {
     acf <- test_fbm_acf(alpha, dt, N)
     X <- f + rSnorm(n = 1, acf = acf)
     Nt <- NormalToeplitz(n = N, p = 2)
-    g1 <- c(grad(func = test_logdens, x = mu, alpha = alpha, X = X), 
+    g1 <- c(grad(func = test_logdens, x = mu, alpha = alpha, X = X),
             grad(func = test_logdens, x = alpha, mu = mu, X = X))
     dz <- cbind(-test_drift_grad(mu, N), rep(0, N))
     dacf <- cbind(rep(0, N), test_fbm_acf_grad(alpha, dt, N))
     g2 <- Nt$grad(z = X - f, dz = dz, acf = acf, dacf = dacf)
-
     expect_equal(g1, g2)
   })
 })
-
--- tests/testthat/test-NormalToeplitz-hess.R
+++ tests/testthat/test-NormalToeplitz-hess.R
@@ -6,7 +6,7 @@ context("NormalToeplitz - Hessian matrix.")
 
 nrep <- 10
 test_that("The GSchur algorithm returns the correct density", {
-  replicate(n = nrep, expr = {  
+  replicate(n = nrep, expr = {
     test_logdens <- function(mu, alpha, X) {
       f <- test_drift_func(mu, N)
       acf <- test_fbm_acf(alpha, dt, N)
@@ -16,7 +16,6 @@ test_that("The GSchur algorithm returns the correct density", {
     test_logdens_grad <- function(mu, alpha, X) {
       grad(func = test_logdens, x = mu, alpha = alpha, X = X)
     }
-    
     N <- round(abs(rnorm(n = 1, mean = 200, sd = 5)))
     p <- 2
     dt <- runif(1,0,1)
@@ -32,14 +31,12 @@ test_that("The GSchur algorithm returns the correct density", {
     d2z[,1,1] <- -test_drift_hess(mu, N)
     d2acf <- array(data = 0, dim = c(N, p, p))
     d2acf[,2,2] <- test_fbm_acf_hess(alpha, dt, N)
-    
     h11 <- hessian(func = test_logdens, x = mu, alpha = alpha, X = X)
     h12 <- h21 <- grad(func = test_logdens_grad, x = alpha, mu = mu, X = X)
     h22 <- hessian(func = test_logdens, x = alpha, mu = mu, X = X)
     hmat1 <- matrix(c(h11, h12, h21, h22), 2, 2)
     hmat2 <- Nt$hess(z = X - f, dz = dz, d2z = d2z, acf = acf, dacf = dacf, d2acf = d2acf)
-    
-    expect_equal(max(abs(hmat1 - hmat2)), 0, tolerance = 1e-4)
+    expect_equal(hmat1, hmat2, tolerance = 1e-4)
   })
 })
 
